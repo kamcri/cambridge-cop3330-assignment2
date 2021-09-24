6a290823d6fe2fd96b6ad65a76e0231c370d06b2
kamcri <89857199+kamcri@users.noreply.github.com>
2021-09-24 13:50:01 -0400

    initial commit

--- src/main/java/ex25/App.java
+++ src/main/java/ex25/App.java
@@ -3,10 +3,42 @@
  *  Copyright 2021 Kameryn Cambridge
  */
 package ex25;
+import java.util.Scanner;
 public class App
 {
+    static void passwordValidator(String password)
+    {
+        /*check for number of characters
+        int length = password.length();
+
+        boolean numbersOnly = false, lettersOnly = false;
+
+        if(numbersOnly == true && length < 8)
+        {
+
+        } */
+
+        /* Uses a single output statement.
+        if(password == numbers only && less than 8 characters)
+        {
+                very weak
+        }
+        else if(password == letters only && less than 8 characters)
+        {
+                weak
+        }
+        else if(password == >= 1 number && >= 8 characters)
+        {
+                strong
+        }
+        else
+                very strong
+         */
+    }
     public static void main( String[] args )
     {
-        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a password: ");
+        String password = sc.next();
     }
 }
---
+++ src/main/java/ex27/Main.java
@@ -0,0 +1,90 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Kameryn Cambridge
+ */
+package ex27;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.String;
+public class Main
+{
+    static String valid = "";
+    static boolean validateName(String Name)
+    {
+        boolean result = true;
+
+        if(Name.length() < 2)
+        {
+            valid = valid + "The first name must be at least 2 characters long.\n";
+            result = false;
+        }
+        if(Name.length() == 0)
+        {
+            valid = valid + "The first name must be filled in.\n";
+            result = false;
+        }
+        return result;
+    }
+    static boolean validateID(String empID)
+    {
+        boolean result = true;
+
+        Pattern p = Pattern.compile("[a-zA-Z]{2}[-]\d{4}");
+        Matcher m = p.matcher(empID);
+        if(!empID.equals(p))
+        {
+            valid = valid + "The employee ID must be in the format of AA-1234.\n";
+            return false;
+        }
+        return true;
+    }
+    static boolean validateZip(String zipCode)
+    {
+        boolean result = true;
+        try
+        {
+            int zip = Integer.parseInt(zipCode);
+            if(zipCode.length() != 5)
+            {
+                valid = valid + "The zipcode must be a 5 digit number.\n";
+                result = false;
+            }
+        }
+        catch (Exception e)
+        {
+            valid = valid + "The zipcode must be a 5 digit number.\n";
+            result = false;
+        }
+        return result;
+    }
+    public static void validateAll(String first, String last, String ID, String zip)
+    {
+        boolean firstN = validateName(first);
+        boolean lastN = validateName(last);
+        boolean emp = validateID(ID);
+        boolean zipCode = validateZip(zip);
+
+        if(firstN && lastN && emp && zipCode)
+        {
+            valid = "There were no errors found.\n";
+        }
+        System.out.println(valid);
+    }
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        String fName, lName, empID, zipCode;
+
+        System.out.println("Enter the first name:");
+        fName = sc.next();
+        System.out.println("Enter the last name:");
+        lName = sc.next();
+        System.out.println("Enter the ZIP code:");
+        zipCode = sc.next();
+        System.out.println("Enter the employee ID:");
+        empID = sc.next();
+
+        validateAll(fName, lName, empID, zipCode);
+    }
+}
