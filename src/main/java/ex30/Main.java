c5cb86280d50989a0088d586c1b92a5dc836ea1c
kamcri <89857199+kamcri@users.noreply.github.com>
2021-09-25 00:14:01 -0400

    initial commit

--- src/main/java/ex30/Main.java
+++ src/main/java/ex30/Main.java
@@ -3,14 +3,18 @@
  *  Copyright 2021 Kameryn Cambridge
  */
 package ex30;
+import javax.swing.JOptionPane;
 public class Main
 {
     public static void main(String[] args)
     {
+        String b = JOptionPane.showInputDialog("Enter a number for your base:" );
+        JOptionPane.showMessageDialog(null, "Multiplication table for " +b);
+        int base =Integer.parseInt(b);
         int r, c;
-        for(r = 1; r <= 12; r++)
+        for(r = 1; r <= base; r++)
         {
-            for(c = 1; c <= 12; c++)
+            for(c = 1; c <= base; c++)
             {
                 System.out.printf("%4d", r*c);
             }
