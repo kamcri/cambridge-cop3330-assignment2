e6c1371d79476f36a7f30cf854d2084451f163ef
kamcri <89857199+kamcri@users.noreply.github.com>
2021-09-26 21:43:16 -0400

    initial commit

---
+++ src/main/java/ex34/Main.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Kameryn Cambridge
+ */
+package ex34;
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String[] contestants = new String[100];
+        String name;
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+
+        while (true) {
+            System.out.print("Enter a name: ");
+            name = sc.nextLine();
+            contestants[i] = name;
+            i += 1;
+            if (name.isEmpty()) {
+                i -=1;
+                winner(contestants, i);
+                break;
+            }
+        }
+    }
+    public static void winner(String[] contestants, int i)
+    {
+        Random random = new Random();
+        int winner = random.nextInt(i);
+        System.out.println("The winner is..." +contestants[winner]+"!");
+    }
+}
