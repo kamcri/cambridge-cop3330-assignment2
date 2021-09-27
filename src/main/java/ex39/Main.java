b2094b9d5e23b272cd97a76111c66b48677d41f6
kamcri <89857199+kamcri@users.noreply.github.com>
2021-09-27 16:11:11 -0400

    initial commit

---
+++ src/main/java/ex39/Main.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Kameryn Cambridge
+ */
+package ex39;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+        HashMap<String, String> myMap = new HashMap<>();
+
+        myMap.put("John Johnson", "Manager");
+        myMap.put("Tou Xiong", "Software Engineer");
+        myMap.put("Michaela Michaelson", "District Manager");
+        myMap.put("Jake Jacobson", "Programmer");
+        myMap.put("Jacquelyn Jackson", "DBA");
+        myMap.put("Sally Webber", "Web Developer");
+
+        displayMap(myMap);
+    }
+    private static void displayMap(Map<String, String> map)
+    {
+        Set<String> positions = map.keySet();
+
+        TreeSet<String> sortedKeys = new TreeSet<>(positions);
+
+        System.out.printf("%nMap contains:%nName\t\t\t\t|Position\t\t\t |Separation Date%n");
+        System.out.println("--------------------|--------------------|-----------------");
+        for(String position : sortedKeys)
+        {
+            System.out.printf("%-20s|%-20s|%n", position, map.get(position));
+        }
+    }
+}
+
